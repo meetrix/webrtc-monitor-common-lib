1e26904662a713996929a0df5de7e777218d1d45
Buddhika Jayawardhana <buddhika.anushka@gmail.com>
2022-01-07 08:29:33 +0000

    Show stats on dashboard

--- dist/socketEvents.js
+++ dist/socketEvents.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SOCKET_ROOM_JOIN = exports.SOCKET_CLIENT_LEFT = exports.SOCKET_CLIENT_JOINED = void 0;
+exports.SOCKET_REPORT_STATS = exports.SOCKET_ROOM_JOIN = exports.SOCKET_CLIENT_LEFT = exports.SOCKET_CLIENT_JOINED = void 0;
 exports.SOCKET_CLIENT_JOINED = 'client/joind';
 exports.SOCKET_CLIENT_LEFT = 'client/left';
 exports.SOCKET_ROOM_JOIN = 'room/join';
+exports.SOCKET_REPORT_STATS = 'stats';
 //# sourceMappingURL=socketEvents.js.map
